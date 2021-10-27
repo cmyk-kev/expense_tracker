44e6695ea6077ad76613e6a274e96e88f5080c9a
Kevin da Silva Felix <kevinsfelix@outlook.com>
2021-10-26 21:43:19 -0300

    arquivos do projeto

---
+++ src/data/items.ts
@@ -0,0 +1,11 @@
+import { Item } from '../types/Item';
+
+
+
+export const items: Item[] = [
+  { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2021, 9, 16), category: 'food', title: 'BurgerKink', value: 28 },
+  { date: new Date(2021, 9, 17), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+  { date: new Date(2021, 10, 18), category: 'salary', title: 'Salario ACME', value: 4500 }
+];
+
